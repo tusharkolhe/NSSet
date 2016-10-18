f5cd1c10a56ba9b54216654c2965aea7bfc96859
Felix ITs 01 <felixits01@felixs-macbook-pro-2.local>
2016-10-18 11:34:42 +0530

    Initial Commit

---
+++ TKNSSet/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  TKNSSet
+//
+//  Created by Felix ITs 01 on 18/10/16.
+//  Copyright © 2016 Tushar. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
